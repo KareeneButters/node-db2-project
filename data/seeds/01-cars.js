7f0eb81ed1764090c0205416357919ba6b99c72b
Kareene Butler <Kareenebutler5@gmail.com>
2024-08-03 13:30:16 -0400

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,33 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    }, 
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 115000,
+        title: 'salvage',
+    },
+    {
+
+        vin: '1111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+        title: 'clean',
+    },
+]
+
+exports.seed = function(knex) {
+    return knew('cars')
+        .truncate().then(() => {
+            return knew('cars').insert(cars)
+    })
+}
